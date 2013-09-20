2670747e8c71352930faa1bf3ace1b301eb8a1c1
sam <sam@smscentral.com.au>
2013-09-20 13:24:37 +1000

    Nodejs example

---
+++ smscentral.js
@@ -0,0 +1,20 @@
+var request     = require('request'),
+    url         = 'https://my.smscentral.com.au/wrapper/sms',
+    username    = 'sam',
+    password    = 'password',
+    action      = 'send',
+    originator  = 'shared',
+    recipient   = '0434390783',
+    messagetext = 'Hello from SMSCentral!';
+
+
+request.post(url, {form:{
+            'USERNAME'  : username,
+            'PASSWORD'  : password,
+            'ACTION'    : action,
+            'ORIGINATOR': originator,
+            'RECIPIENT' : recipient,
+            'MESSAGE_TEXT' : messagetext
+        }},function(e, r, body){
+            console.log(body);
+        });
